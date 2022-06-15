790493f4063d873f34688ad4e6bbcec58606a758
MichaelLie08 <85691302+MichaelLie08@users.noreply.github.com>
2022-06-15 12:57:00 +0800

    home selesai

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:proyek_chatting/chatting.dart';
 import 'package:proyek_chatting/coba.dart';
+import 'package:proyek_chatting/screen/home_screen.dart';
 
 import 'firebase_options.dart';
 
@@ -11,66 +12,15 @@ void main() async {
   await Firebase.initializeApp();
   runApp(MaterialApp(
     title: "Chatting",
-    home: MyApp(),
+    home: HomeScreen(),
   ));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String username1 = "romario1";
-  String username2 = "romario2";
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
+class Chat_app extends StatelessWidget {
+  const Chat_app({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Chatting")),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // _jumlah++;
-          // final dtBaru = itemCatatan(itemJudul: _jumlah.toString(), itemIsi: "333");
-          // Database.tambahData(item: dtBaru);
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => COBAA(
-                        username1: username1,
-                        username2: username2,
-                      )));
-        },
-        backgroundColor: Colors.blueGrey,
-        child: Icon(
-          Icons.chat_bubble,
-          color: Colors.white,
-          size: 32,
-        ),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(8.0),
-        margin: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 0),
-        child: Column(
-          children: [
-            TextField(
-              // controller: ctrSearch,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10), borderSide: BorderSide(color: Colors.blue)),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return MaterialApp(title: "Chat App", home: HomeScreen());
   }
 }
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:proyek_chatting/pages/chat_page.dart';
+import 'package:proyek_chatting/pages/contact_page.dart';
+import 'package:proyek_chatting/pages/profile_page.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  final ValueNotifier<int> index = ValueNotifier(0);
+  final ValueNotifier<String> title = ValueNotifier("Message");
+
+  final pages = const [ChatPage(), ContactPage(), ProfilePage()];
+
+  final pageTitles = const [
+    'Message',
+    'Contact',
+    'Profile',
+  ];
+
+  void _onNavigationItemSelector(indexes) {
+    title.value = pageTitles[indexes];
+    index.value = indexes;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: ValueListenableBuilder(
+          valueListenable: title,
+          builder: (BuildContext context, String value, _) {
+            return Center(
+              child: Text(
+                value,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: index,
+        builder: (BuildContext context, int value, _) {
+          return pages[value];
+        },
+      ),
+      bottomNavigationBar: _BottomNavigationBar(
+        onItemSelected: _onNavigationItemSelector,
+      ),
+    );
+  }
+}
+
+class _BottomNavigationBar extends StatefulWidget {
+  const _BottomNavigationBar({Key? key, required this.onItemSelected})
+      : super(key: key);
+
+  final ValueChanged<int> onItemSelected;
+
+  @override
+  State<_BottomNavigationBar> createState() => _BottomNavigationBarState();
+}
+
+class _BottomNavigationBarState extends State<_BottomNavigationBar> {
+  var selectedindex = 0;
+  void handleItemSelected(int index) {
+    setState(() {
+      selectedindex = index;
+    });
+    widget.onItemSelected(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: false,
+      bottom: true,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _NavigationBarItem(
+              index: 0,
+              label: 'Chat',
+              icon: CupertinoIcons.bubble_left_bubble_right_fill,
+              isSelected: (selectedindex == 0),
+              onTap: handleItemSelected),
+          _NavigationBarItem(
+              index: 1,
+              label: 'Contact',
+              icon: CupertinoIcons.person_2_fill,
+              isSelected: (selectedindex == 1),
+              onTap: handleItemSelected),
+          _NavigationBarItem(
+              index: 2,
+              label: 'Profile',
+              icon: CupertinoIcons.profile_circled,
+              isSelected: (selectedindex == 2),
+              onTap: handleItemSelected),
+        ],
+      ),
+    );
+  }
+}
+
+class _NavigationBarItem extends StatelessWidget {
+  const _NavigationBarItem({
+    Key? key,
+    required this.index,
+    required this.label,
+    required this.icon,
+    required this.onTap,
+    this.isSelected = false,
+  }) : super(key: key);
+
+  final ValueChanged<int> onTap;
+  final int index;
+  final String label;
+  final IconData icon;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        HitTestBehavior.opaque;
+        onTap(index);
+      },
+      child: SizedBox(
+        height: 70,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(icon, size: 20, color: isSelected ? Colors.blue : null),
+            SizedBox(height: 8),
+            Text(
+              label,
+              style: const TextStyle(fontSize: 11),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
